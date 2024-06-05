c0031ff869863fdac6c20df6d219953bc6028eee
IYuliia <catnew2012@gmail.com>
2024-06-05 17:24:23 +0200

    add favorite routers and controllers

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,4 +1,5 @@
 import { listAllRecipesService } from '../services/recipesServices.js';
+import Recipe from '../models/Recipe.js';
 
 export const getAllOwnRecipes = async (req, res, next) => {
   try {
@@ -9,6 +10,59 @@ export const getAllOwnRecipes = async (req, res, next) => {
     const skip = (page - 1) * limit;
     const settings = { skip, limit };
     const result = await listAllRecipesService({ filter, fields, settings });
+    res.json({ result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addToFavorites = async (req, res, next) => {
+  try {
+    const { recipeId } = req.body; 
+    const { _id: userId } = req.user;
+
+    const recipe = await Recipe.findOne({ _id: recipeId, owner: userId });
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found or doesn't belong to the user" });
+    }
+
+    recipe.favorite = true;
+    await recipe.save();
+
+    res.status(200).json({ message: 'Recipe added to favorites successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const removeFromFavorites = async (req, res, next) => {
+  try {
+    const { recipeId } = req.body; 
+    const { _id: userId } = req.user;
+
+    const recipe = await Recipe.findOne({ _id: recipeId, owner: userId });
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found or doesn't belong to the user" });
+    }
+
+    recipe.favorite = false;
+    await recipe.save();
+
+    res.status(200).json({ message: 'Recipe removed from favorites successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getFavoriteRecipes = async (req, res, next) => {
+  try {
+    const { _id: owner } = req.user;
+    const filter = { owner, favorite: true }; 
+    const fields = "-createdAt -updatedAt";
+    const result = await listAllRecipesService({ filter, fields });
     res.json({ result });
   } catch (error) {
     next(error);
--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { getAllOwnRecipes } from '../controllers/recipesController.js';
+import { getAllOwnRecipes, addToFavorites, removeFromFavorites, getFavoriteRecipes } from '../controllers/recipesController.js';
 import authenticate from '../middlewares/authenticate.js';
 
 const recipesRouter = express.Router();
 
 recipesRouter.get('/', authenticate, getAllOwnRecipes);
+
+recipesRouter.post("/favorites", authenticate, addToFavorites);
+
+recipesRouter.delete("/favorites", authenticate, removeFromFavorites);
+
+recipesRouter.get("/favorites", authenticate, getFavoriteRecipes);
 
 export default recipesRouter;
