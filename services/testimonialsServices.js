40bc57053658c0a673f772b0580dcb291ee599ea
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-06-06 19:19:48 +0300

    update testimonials endpoint

--- services/testimonialsServices.js
+++ services/testimonialsServices.js
@@ -1,3 +1,9 @@
 import Testimonial from "../models/Testimonial.js";
 
-export const getAll = () => Testimonial.find();
+export const getAll = (search = {}) => {
+    const { filter = {}, fields = '', settings = {} } = search;
+    return Testimonial.find(filter, fields, settings);
+};
+    
+
+export const countTestimonials = () => Testimonial.countDocuments();
