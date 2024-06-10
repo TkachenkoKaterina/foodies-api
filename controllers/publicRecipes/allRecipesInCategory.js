837497a80b7906334fad116c298038789b2be391
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-06-11 01:30:27 +0300

    min fix-filter response

--- controllers/publicRecipes/allRecipesInCategory.js
+++ controllers/publicRecipes/allRecipesInCategory.js
@@ -1,19 +1,15 @@
-// import ingId from '../../helpers/ingNameToId.js';
-import { Types } from 'mongoose';
 import publicServices from '../../services/publicRecipesServ/index.js';
-// import { ObjectId } from 'mongodb';
 
 const allRecipesInCategory = async (req, res, next) => {
   const { category } = req.params;
   const { ingredient, area, page = 1, limit = 12 } = req.query;
   const skip = (page - 1) * limit;
   const filter = {};
-  // const ObjectId = mongoose.Types.ObjectId;
   if (category) filter.category = category;
   if (ingredient) filter.ingredients = { $elemMatch: { id: ingredient } };
   if (area) filter.area = area;
 
-  const fields = 'title category area description owner thumb ingredients';
+  const fields = 'title description owner thumb';
   const settings = { skip, limit };
 
   const { total, data } = await publicServices.getFilteredRecipes({
