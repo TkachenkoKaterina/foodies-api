6565bbb1e1287136e8d7f1d462d613b4587a0837
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-06-12 23:35:06 +0300

    add settings for paginations of categories

--- controllers/testimonialsControllers.js
+++ controllers/testimonialsControllers.js
@@ -2,7 +2,7 @@ import * as testimonialsService from "../services/testimonialsServices.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
 const getAllTestimonials = async (req, res) => {
-  const { page = 1, limit = 1 } = req.query;
+  const { page = 1, limit = 3 } = req.query;
   const skip = (page - 1) * limit;
   const settings = { skip, limit };
   const total = await testimonialsService.countTestimonials();
--- services/categoriesServices.js
+++ services/categoriesServices.js
@@ -1,3 +1,8 @@
 import Category from "../models/Category.js";
 
-export const getAll = () => Category.find();
+export const getAll = (search = {}) => {
+    const { filter = {}, fields = '', settings = {} } = search;
+    return Category.find(filter, fields, settings);
+};
+
+export const countCategories = () => Category.countDocuments();
