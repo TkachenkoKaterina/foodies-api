5b28c18642c4933ae7c84a998f1101c90fa7f1a6
Dima Tytenko <amgpetronass@gmail.com>
2024-06-13 23:08:30 +0300

    fix favorite recipes

--- docs/paths/user/index.js
+++ docs/paths/user/index.js
@@ -4,35 +4,22 @@ import followUser from './followUser.js';
 import unfollowUser from './unfollowUser.js';
 import followingUser from './followingUser.js';
 import followersUser from './followersUser.js';
-import getFavRecipes from './getFavRecipes.js';
-import addToFavorites from './addToFavorites.js';
-import removeFromFavorites from './removeFromFavorites.js';
 
 export default {
-  paths: {
-    '/users/signup': {
-      ...registerUser,
-    },
-    '/users/signin': {
-      ...loginUser,
-    },
-    '/users/follow/{id}': {
-      ...followUser,
-      ...unfollowUser,
-    },
-    '/users/following': {
-      ...followingUser,
-    },
-    '/users/followers/{id}': {
-      ...followersUser,
-    },
-    '/recipes/favorites': {
-      ...getFavRecipes,
-      
-    },
-    '/recipes/favorites/{id}': {
-      ...addToFavorites,
-      ...removeFromFavorites,
-    },
+  '/users/signup': {
+    ...registerUser,
+  },
+  '/users/signin': {
+    ...loginUser,
+  },
+  '/users/follow/{id}': {
+    ...followUser,
+    ...unfollowUser,
+  },
+  '/users/following': {
+    ...followingUser,
+  },
+  '/users/followers/{id}': {
+    ...followersUser,
   },
 };
