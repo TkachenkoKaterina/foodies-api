0900237000c6e72bfcd5e374bc300fb3abe4d405
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-06-15 00:12:48 +0300

    fix-popular-resp

--- services/publicRecipesServ/getPopular.js
+++ services/publicRecipesServ/getPopular.js
@@ -1,12 +1,12 @@
 import Recipe from '../../models/Recipe.js';
 
-export const getPopularRecipes = async (search = {}) => {
-  const { filter = {}, fields = '', settings = {} } = search;
-  const total = await Recipe.countDocuments();
-  const data = await Recipe.find(filter, fields, settings).sort({ likes: 1 });
+export const getPopularRecipes = async () => {
+  const data = await Recipe.find()
+    .populate('owner', 'name avatar')
+    .sort({ likes: 1 })
+    .limit(4);
 
   return {
-    total,
     data,
   };
 };
